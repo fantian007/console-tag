41a3f05c724b4a21dfd6ad38efae0f31bbd0644f
赵永盛 <1562743134@qq.com>
2024-01-05 12:12:17 +0800

    feat: 检测git仓库

--- src/helpers/git/run-git-command.ts
+++ src/helpers/git/run-git-command.ts
@@ -6,21 +6,36 @@ interface Cb {
   (err: Error | null, output: string): void;
 }
 
-export default function (gitWorkTree: string | undefined, command: string, callback?: Cb) {
+export default function(
+  gitWorkTree: string | undefined,
+  command: string,
+  callback?: Cb
+) {
   const gitCommand = gitWorkTree
-    ? ['git', '--git-dir=' + path.join(gitWorkTree, '.git'), '--work-tree=' + gitWorkTree, command].join(' ')
+    ? [
+        'git',
+        '--git-dir=' + path.join(gitWorkTree, '.git'),
+        '--work-tree=' + gitWorkTree,
+        command,
+      ].join(' ')
     : ['git', command].join(' ');
 
   if (callback) {
-    exec(gitCommand, function (err, stdout) {
+    exec(gitCommand, function(err, stdout) {
       if (err) {
         return callback(err, '');
       }
       callback(null, removeEmptyLines(stdout));
-    })
+    });
 
     return null;
   } else {
-    return removeEmptyLines(`${execSync(gitCommand)}`);
+    try {
+      const r = removeEmptyLines(`${execSync(gitCommand)}`);
+      return r;
+    } catch (error) {
+      console.error((error as Error).message);
+      return '-';
+    }
   }
 }
